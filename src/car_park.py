dbaf49cb01f72acf4d560c0d6a6adc25465b991c
Jon-Paul Fitzgerald <jon.paul.fitz@gmail.com>
2025-11-26 20:30:19 +0800

    Carpark classes finalised

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,8 @@
 class CarPark:
-    pass # this just means nothing... it actually does nothing
-# do this for ALL the other classes!
+    def __init__(self, id, message, is_on):
+        self.id = id
+        self.message = message
+        self.is_on = is_on or []
+
+    def __str__(self):
+        return f"Welcome to {self.location} car park"
--- src/display.py
+++ src/display.py
@@ -1,3 +1,9 @@
 class Display:
-    pass # this just means nothing... it actually does nothing
-# do this for ALL the other classes!
+            def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+                self.location = location
+                self.capacity = capacity
+                self.plates = plates or []
+                self.sensors = sensors or []
+                self.displays = displays or []
+
+            def __str__(self):
